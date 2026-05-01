985dfd6d88102a17d937b79a412fa0656b301727
MarcosR19 <0991153@my.scccd.edu>
2026-05-01 04:44:25 +0000

    lab 14.7

--- rodriguez14_7.m
+++ rodriguez14_7.m
@@ -1,1 +1,23 @@
+% Given constants
+m = 1000;     
+c = 500;
+v0 = 0;
+timeInt = [0 10];
+thrustV = [20000 40000 60000 80000];
 
+% Loop through each thrust value and solve using ode 45
+hold on
+for i = 1:length(thrustV)
+    f = thrustV(i);
+    [t,v] = ode45(@(t,v) (f - c*v)/m, timeInt, v0);
+
+    % Plot values
+    plot(t, v, 'LineWidth', 2)
+
+    % Report values at each N level
+    fprintf("When thrust is at %d N, velocity after 10 s is %.2f m/s\n", f, v(end));
+end
+grid on
+title('Rocket Propulsion Application')
+xlabel('Time(s)')
+ylabel('Velocity(m/s)')
